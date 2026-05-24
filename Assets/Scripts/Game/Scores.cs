d11f740fb964f251d74892f16dd2d3dfc4a7cc48
Yehuda <playzyehuda@gmail.com>
2026-05-24 11:58:36 +0700

    Episode 18

--- Assets/Scripts/Game/Scores.cs
+++ Assets/Scripts/Game/Scores.cs
@@ -1,29 +1,70 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
+
+[System.Serializable]
+public class BestScoreData
+{
+    public int score = 0;
+}
 
 public class Scores : MonoBehaviour
 {
     public Text ScoreText;
+    private bool newBestScore_ = false;
+    private BestScoreData bestScores_ = new BestScoreData();
     private int currentScores_;
+
+    private string bestScoreKey_ = "bsdat";
+
+    private void Awake()
+    {
+        if (BinaryDataStream.Exist(bestScoreKey_))
+        {
+            StartCoroutine(ReadDataFile());
+        
+        }
+    }
+
+    private IEnumerator ReadDataFile()
+    {
+        bestScores_ = BinaryDataStream.Read<BestScoreData>(bestScoreKey_);
+        yield return new WaitForEndOfFrame();
+        Debug.Log("Read Best Scores = " + bestScores_.score);
+    }
     void Start()
     {
         currentScores_ = 0;
+        newBestScore_ = false;
         UpdateScoreText();
     }
 
     private void OnEnable()
     {
         GameEvents.AddScores += AddScores;
+        GameEvents.GameOver += SaveBestScores;
     }
 
     private void OnDisable()
     {
         GameEvents.AddScores -= AddScores;
+        GameEvents.GameOver -= SaveBestScores;
+    }
+
+    public void SaveBestScores(bool newBestScores)
+    {
+        BinaryDataStream.Save<BestScoreData>(bestScores_, bestScoreKey_);
     }
 
     private void AddScores(int scores)
     {
         currentScores_ += scores;
+        if(currentScores_ > bestScores_.score)
+        {
+            newBestScore_ = true;
+            bestScores_.score = currentScores_;
+        }
+
         UpdateScoreText();
     }
 
---
+++ Assets/Scripts/Utility/BinaryDataStream.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
+public class BinaryDataStream 
+{
+    public static void Save<T>(T serializedObject, string fileName)
+    {
+        string path = Application.persistentDataPath + "/saves/";
+        Directory.CreateDirectory(path);
+
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream fileStream = new FileStream(path + fileName + ".dat", FileMode.Create);
+
+        try
+        {
+            formatter.Serialize(fileStream, serializedObject);
+        }
+        catch (SerializationException e)
+        {
+            Debug.LogError("Save failed. Error: " + e.Message);
+        }
+        finally
+        {
+            fileStream.Close();
+        }
+    }
+
+    public static bool Exist(string fileName)
+    {
+        string path = Application.persistentDataPath + "/saves/";
+        string fullFileName = fileName + ".dat";
+        return File.Exists(path + fullFileName);
+    }
+
+    public static T Read<T>(string fileName)
+    {
+        string path = Application.persistentDataPath + "/saves/";
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream fileStream = new FileStream(path + fileName + ".dat", FileMode.Open);
+        T returnType = default(T);
+
+        try
+        {
+            returnType = (T)formatter.Deserialize(fileStream);
+        }
+        catch (SerializationException e)
+        {
+            Debug.LogError("Read failed. Error: " + e.Message);
+        }
+        finally
+        {
+            fileStream.Close();
+        }
+
+        return returnType;
+    }
+}
