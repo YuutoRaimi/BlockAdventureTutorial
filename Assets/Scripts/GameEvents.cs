d03ea1074f9d8a268f494390cd1c2a319e259b7b
Yehuda <playzyehuda@gmail.com>
2026-05-24 09:51:57 +0700

    Episode 16

--- Assets/Scripts/GameEvents.cs
+++ Assets/Scripts/GameEvents.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 
 public class GameEvents : MonoBehaviour
 {
+    public static Action<bool> GameOver;
     public static Action<int> AddScores;
     public static Action CheckIfShapeCanBePlaced;
 
